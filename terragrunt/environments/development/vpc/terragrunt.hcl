abb4ce5736b8ce38960c2cb2cc068a3a693c7801
majoraw <wellingtonmajora@yahoo.com>
2024-10-06 13:15:56 +0100

    Added workflow to provision dev env infra

--- terragrunt/environments/development/vpc/terragrunt.hcl
+++ terragrunt/environments/development/vpc/terragrunt.hcl
@@ -4,10 +4,6 @@ include "root" {
 
 terraform {
   source =  "../../../modules//vpc"
-  extra_arguments "common_vars" {
-    commands = get_terraform_commands_that_need_vars()
-    required_var_files = ["${get_parent_terragrunt_dir()}/common.tfvars"]
-  }
 }
 
 inputs = {
--- terragrunt/modules/vpc/vars.tf
+++ terragrunt/modules/vpc/vars.tf
@@ -1,24 +1,24 @@
 variable "env" {
   type        = string
-  description = "run time environment. e.g: stage, prod, dev"
+  description = "development"
 }
 
 variable "region" {
   type = string
-  description = "region. e.g: us-west-2"
+  description = "eu-west-2"
 }
 
 variable "cidr" {
   type = string
-  description = "cidr for network, e.g: 10.0.0.0/16"
+  description = "10.0.0.0/16"
 }
 
 variable "private_subnets" {
   type = list(string)
-  description = "list of private subnets: [10.0.1.0/24, 10.0.2.0/24,...]"
+  description = "[10.0.1.0/24, 10.0.2.0/24, 10.0.3.0/24]"
 }
 
 variable "public_subnets" {
   type = list(string)
-  description = "list of public subnets: [10.0.201.0/24, 10.0.202.0/24,...]"
+  description = "[10.0.101.0/24, 10.0.102.0/24, 10.0.103.0/24]"
 }
